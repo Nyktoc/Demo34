21f9633301fa4974ed178531bc1673f2013f4bed
Nyktoc <168814023+Nyktoc@users.noreply.github.com>
2026-05-18 02:15:17 +0300

    Add files via upload

---
+++ demochka/demochka/CurrentUser.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace demochka
+{
+    public static class CurrentUser
+    {
+        public static string Login { get; set; } = "";
+        public static string Role { get; set; } = "";
+        public static string FirstName { get; set; } = "";
+        public static string LastName { get; set; } = "";
+        public static string Patronymic { get; set; } = "";
+
+        public static string FullName
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(LastName) && string.IsNullOrEmpty(FirstName))
+                    return "Гость";
+
+                string fullName = LastName + " " + FirstName;
+                if (!string.IsNullOrEmpty(Patronymic))
+                    fullName += " " + Patronymic;
+
+                return fullName.Trim();
+            }
+        }
+
+        public static bool IsAuthenticated
+        {
+            get { return Login != "" && Role != "Гость"; }
+        }
+    }
+}
